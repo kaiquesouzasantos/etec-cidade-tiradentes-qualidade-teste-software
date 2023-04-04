1858ae2a21d5166067975cee4bbbcf8e99571d55
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-04-04 13:34:16 -0300

    Update casosTest.list.js

--- exercicios/02/front-end/assets/js/test/casosTest.list.js
+++ exercicios/02/front-end/assets/js/test/casosTest.list.js
@@ -10,5 +10,5 @@ export const CASOS_TESTE = [
     new CasoTeste('Consulta taxa 7,5%', 'teste', 23000, CPF_TESTE, '7.5%'),
     new CasoTeste('Consulta taxa 15%', 'teste', 34000, CPF_TESTE, '15%'),
     new CasoTeste('Consulta taxa  22,5%', 'teste', 46000, CPF_TESTE, '22.5%'),
-    new CasoTeste('Consulta taxa 27,5%', 'teste', 60000, CPF_TESTE, '25.5%')
+    new CasoTeste('Consulta taxa 27,5%', 'teste', 60000, CPF_TESTE, '27.5%')
 ]
