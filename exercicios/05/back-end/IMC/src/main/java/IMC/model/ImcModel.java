d779fe0bd844d57583fedeb396bb92f5621fcc57
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-09-19 18:33:26 -0300

    Update ImcModel.java

--- exercicios/05/back-end/IMC/src/main/java/IMC/model/ImcModel.java
+++ exercicios/05/back-end/IMC/src/main/java/IMC/model/ImcModel.java
@@ -7,6 +7,6 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @SuperBuilder
 public class ImcModel {
-    private String nome, estadp;
+    private String nome, estado;
     private Double peso, altura, imc;
 }
