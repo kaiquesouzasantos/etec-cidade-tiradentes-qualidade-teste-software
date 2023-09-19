cbbaa713d67dbc6257f5d5510ad5b190dc7b7e6c
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-09-19 04:31:26 -0300

    Add files via upload

---
+++ exercicios/04/back-end/AliquotaAnual/src/test/java/AliquotaAnual/model/AliquotaModelTest.java
@@ -0,0 +1,40 @@
+package AliquotaAnual.model;
+
+import AliquotaAnual.util.AliquotaUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class AliquotaModelTest {
+    @Test
+    public void construct() {
+        var expected = AliquotaUtil.factoryModel();
+
+        assertAll(
+                () -> assertEquals(expected.getNome(), AliquotaUtil.NOME),
+                () -> assertEquals(expected.getCpf(), AliquotaUtil.CPF),
+                () -> assertEquals(expected.getRendimentos(), AliquotaUtil.RENDIMENTOS)
+        );
+    }
+
+    @Test
+    public void update() {
+        var expected = AliquotaUtil.factoryModel();
+
+        expected.setNome(AliquotaUtil.NOME_ALTERNATIVO);
+        expected.setCpf(AliquotaUtil.CPF_ALTERNATIVO);
+        expected.setRendimentos(AliquotaUtil.RENDIMENTOS_ALTERNATIVO);
+
+        assertAll(
+                () -> assertEquals(expected.getNome(), AliquotaUtil.NOME_ALTERNATIVO),
+                () -> assertEquals(expected.getCpf(), AliquotaUtil.CPF_ALTERNATIVO),
+                () -> assertEquals(expected.getRendimentos(), AliquotaUtil.RENDIMENTOS_ALTERNATIVO),
+
+                () -> assertNotEquals(expected.getNome(), AliquotaUtil.NOME),
+                () -> assertNotEquals(expected.getCpf(), AliquotaUtil.CPF),
+                () -> assertNotEquals(expected.getRendimentos(), AliquotaUtil.RENDIMENTOS)
+        );
+    }
+}
